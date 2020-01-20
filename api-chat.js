999a911f0865f4c02fc093de8697bbdb5f38a0aa
moravecmatous <moravecmatous@porg.cz>
2020-01-20 17:40:12 +0100

    commit 1

--- api-chat.js
+++ api-chat.js
@@ -5,15 +5,14 @@ const entities = new Entities();
 let msgs = new Array();
 
 exports.apiChat = function (req, res) {
-    let q = url.parse(req.url, true);
-    if (q.pathname == "/chat/listmsgs") { //msgs...globalni promenna typu pole deklarovana na zacatku tohoto zdroje
+    if (req.pathname == "/chat/listmsgs") { //msgs...globalni promenna typu pole deklarovana na zacatku tohoto zdroje
         res.writeHead(200, {
             "Content-type": "application/json"
         });
         let obj = {};
         obj.messages = msgs;
         res.end(JSON.stringify(obj));
-    } else if (q.pathname == "/chat/addmsg") {
+    } else if (req.pathname == "/chat/addmsg") {
         let data = "";
         req.on('data', function (chunk) {
             try {
--- api-users.js
+++ api-users.js
@@ -21,51 +21,38 @@ function zamixujHeslo(pw) {
 }
 
 exports.apiUsers = function (req, res) {
-    let q = url.parse(req.url, true);
-    if (q.pathname.endsWith("/list")) {
+    if (req.pathname.endsWith("/list")) {
         res.writeHead(200, {
             "Content-type": "application/json"
         });
         let obj = {};
         obj.users = users;
         res.end(JSON.stringify(obj));
-    } else if (q.pathname.endsWith("/add")) {
-        let data = "";
-        req.on('data', function (chunk) {
-            try {
-                data += chunk;
-            } catch (e) {
-                console.error(e);
-            }
-        })
-        req.on('end', function () {
-            if (data) {
-                let body = JSON.parse(data);
-                res.writeHead(200, {
-                    "Content-type": "application/json"
-                });
-                let obj = {}
-                obj.login = entities.encode(body.login);
-                let userExists = false;
-                for (let u of users) {
-                    if (u.login === obj.login) {
-                        userExists = true;
-                        break;
-                    }
-                }
-                if (userExists) {
-                    obj.error = "User already exists.";
-                } else {
-                    obj.name = entities.encode(body.name);
-                    obj.password = zamixujHeslo(entities.encode(body.password));
-                    obj.email = entities.encode(body.email);
-                    users.push(obj);
-                    fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
-                }
-                res.end(JSON.stringify(obj));
-            };
+    } else if (req.pathname.endsWith("/add")) {
+        res.writeHead(200, {
+            "Content-type": "application/json"
         });
-    } else if (q.pathname.endsWith("/login")) {
+        let obj = {}
+        obj.login = req.parameters.login;
+        let userExists = false;
+        for (let u of users) {
+            if (u.login === obj.login) {
+                userExists = true;
+                break;
+            }
+        }
+        if (userExists) {
+            obj.error = "User already exists.";
+        } else {
+            obj.name = req.parameters.name;
+            obj.password = req.parameters.password;
+            obj.email = req.parameters.email;
+            users.push(obj);
+            fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
+        }
+        res.end(JSON.stringify(obj));
+
+    } else if (req.pathname.endsWith("/login")) {
         let data = "";
         req.on('data', function (chunk) {
             try {
@@ -81,12 +68,12 @@ exports.apiUsers = function (req, res) {
                     "Content-type": "application/json"
                 });
                 let obj = {};
-                let login = entities.encode(body.login);
+                let login = req.parameters.login;
                 let found = false;
                 for (let u of users) {
                     if (u.login === login) {
                         found = true;
-                        if (u.password === entities.encode(body.password)) {
+                        if (u.password === req.parameters.password) {
                             obj.name = u.name;
                         } else {
                             obj.error = "invalid password";
